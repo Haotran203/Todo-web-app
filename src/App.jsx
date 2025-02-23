c0480e8900f7701e7f9c8d2cbe9be0e4a7aed826
Haotran203 <quanghaotran303@gmail.com>
2025-02-23 16:49:51 +0700

    Add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import Button from '@mui/material/Button'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-import HomeIcon from '@mui/icons-material/Home';
-import { pink } from '@mui/material/colors';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
 
 function App() {
 
+  function ModeToggle() {
+    const { mode, setMode } = useColorScheme()
+    return (
+      <Button
+        onClick={() => {
+          setMode(mode === 'light' ? 'dark' : 'light')
+        }}
+      >
+        {mode === 'light' ? 'Turn dark' : 'Turn light'}
+      </Button>
+    );
+  }  
+
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>Tran Quang Hao</div>
+      <Typography variant="body2" color="text.secondary">Text Typography</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
