72d682dab7997c2897e7dc86875ec943a4bae405
Haotran203 <quanghaotran303@gmail.com>
2025-02-27 21:23:15 +0700

    Design Appbar #1

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -8,7 +8,6 @@ import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import Select from '@mui/material/Select'
-import { CssBaseline } from '@mui/material'
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
