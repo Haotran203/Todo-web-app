90a902913eeb2165b2cbd9f7babf7138054babfc
Haotran203 <quanghaotran303@gmail.com>
2025-03-03 21:49:51 +0700

    Fomat structure project

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -0,0 +1,48 @@
+import AttachmentIcon from '@mui/icons-material/Attachment'
+import CommentIcon from '@mui/icons-material/Comment'
+import GroupIcon from '@mui/icons-material/Group'
+import Button from '@mui/material/Button'
+import { Card as MuiCard } from '@mui/material' // Rename Card component to MuiCard. Because funtion Card is already exist
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+
+function Card({ temporaryHideMedia }) {
+  if (temporaryHideMedia) {
+    return (
+      <MuiCard sx={{
+        cursor: 'pointer',
+        boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+        overflow: 'unset'
+      }}>
+        <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+          <Typography>Card test 01</Typography>
+        </CardContent>
+      </MuiCard>
+    )
+  }
+  return (
+    <MuiCard sx={{
+      cursor: 'pointer',
+      boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+      overflow: 'unset'
+    }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+        title="green iguana"
+      />
+      <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+        <Typography>Hello World</Typography>
+      </CardContent>
+      <CardActions sx={{ p: '0 4px 8px 4px' }}>
+        <Button size="small" startIcon={<GroupIcon />}>20</Button>
+        <Button size="small" startIcon={<CommentIcon />}>15</Button>
+        <Button size="small" startIcon={<AttachmentIcon />}>05</Button>
+      </CardActions>
+    </MuiCard>
+  )
+}
+
+export default Card
