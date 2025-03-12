e60b690173f3916d71c7a621a655cbb27815494d
Haotran203 <quanghaotran303@gmail.com>
2025-03-12 21:59:00 +0700

    Completed FrontEnd

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -4,3 +4,13 @@ export const capitalizeFirstLetter = (val) => {
   if (!val) return ''
   return `${val.charAt(0).toUpperCase()}${val.slice(1)}`
 }
+
+// Hàm tạo ra một Placeholder Card rỗng để xử lý trường hợp Column rỗng
+export const generatePlaceholderCard = (column) => {
+  return {
+    _id: `${column._id}-placeholer-card`,
+    boardId: column.boardId,
+    columnId: column._id,
+    FE_PlaceholderCard: true
+  }
+}
